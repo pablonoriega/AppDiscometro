6b57fcc78af978de64ae60f9458bc3661ca80bbd
christiannrnx <72551441+christiannrnx@users.noreply.github.com>
2022-03-31 11:00:40 +0200

    clases Favoritos Objetos Perdidos y Registro añadidas

--- app/src/main/java/com/discometro/favoritos/FavoritosFragment.java
+++ app/src/main/java/com/discometro/favoritos/FavoritosFragment.java
@@ -1,4 +1,4 @@
-package com.discometro;
+package com.discometro.favoritos;
 
 import android.os.Bundle;
 
@@ -7,6 +7,8 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.discometro.R;
 
 /**
  * A simple {@link Fragment} subclass.
---
+++ app/src/main/java/com/discometro/objetosPerdidos/ObjetosPerdidosActivity.java
@@ -0,0 +1,16 @@
+package com.discometro.objetosPerdidos;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.discometro.R;
+
+public class ObjetosPerdidosActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_objetos_perdidos);
+    }
+}
