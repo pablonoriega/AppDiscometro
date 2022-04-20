b5d457b721d626daada916cc0dec787f70a352a2
SrNatsu <adriangalan0204@gmail.com>
2022-04-20 21:09:05 +0200

    Perfiles DownTown y Otto

---
+++ app/src/main/java/com/discometro/PerfilDisco/BotonesPerfilDisco.java
@@ -0,0 +1,13 @@
+package com.discometro.PerfilDisco;
+
+import android.view.View;
+
+public interface BotonesPerfilDisco {
+
+    public void showImage(View view);
+
+    public void intentToEmail(View view);
+
+    public void intentToObjetosPerdidos(View view);
+
+}
