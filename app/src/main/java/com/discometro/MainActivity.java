01cdbf91be91fa660daa6759c91b81d067538b0f
SrNatsu <adriangalan0204@gmail.com>
2022-05-15 19:02:16 +0200

    Usuario

--- app/src/main/java/com/discometro/MainActivity.java
+++ app/src/main/java/com/discometro/MainActivity.java
@@ -10,7 +10,9 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
+import com.discometro.PerfilDisco.PerfilDisco;
 import com.discometro.Suscripciones.SuscripcionesFragment;
+import com.discometro.ViewModel.ViewModelMain;
 import com.discometro.VueltaSegura.AgregarVueltaSeguraActivity;
 import com.discometro.VueltaSegura.VueltaSeguraFragment;
 import com.discometro.favoritos.FavoritosFragment;
@@ -24,25 +26,28 @@ public class MainActivity extends AppCompatActivity {
     private SuscripcionesFragment suscripcionesFragment;
     private FavoritosFragment favoritosFragment;
     private PerfilUserFragment perfilUserFragment;
-    private User u;
+    private User user;
+    private static String correo;
+    private ViewModelMain vm;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        vm = new ViewModelMain(getApplication());
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigationView);
         NavController navController = Navigation.findNavController(this, R.id.fragment);
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(R.id.mapsFragment, R.id.vueltaSeguraFragment, R.id.suscripcionesFragment, R.id.favoritosFragment, R.id.perfilUserFragment).build();
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(bottomNavigationView, navController);
-        Intent intent = getIntent();
-         u= intent.getParcelableExtra("usuario");
+        Bundle extras = getIntent().getExtras();
 
-
-
-
-
+        if (extras.getString("usuario") != null) {
+            correo = extras.getString("usuario");
+        }
+        System.out.println(correo + "HOLAAA");
+        user = vm.getUserById(correo);
     }
 
     public void intentToAgregarVueltaSegura(View view) {
@@ -51,6 +56,10 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public User getUser(){
-        return this.u;
+        return user;
+    }
+
+    public String getUserCorreo() {
+        return user.getCorreo();
     }
 }
--- app/src/main/java/com/discometro/Suscripciones/SuscripcionesFragment.java
+++ app/src/main/java/com/discometro/Suscripciones/SuscripcionesFragment.java
@@ -1,5 +1,6 @@
 package com.discometro.Suscripciones;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -14,6 +15,7 @@ import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.discometro.MainActivity;
+import com.discometro.PerfilDisco.PerfilDisco;
 import com.discometro.R;
 import com.discometro.User;
 import com.discometro.ViewModel.ViewModelMain;
@@ -33,6 +35,7 @@ public class SuscripcionesFragment extends Fragment {
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
+    private Intent intent;
 
     // TODO: Rename and change types of parameters
     private String mParam1;
@@ -44,6 +47,7 @@ public class SuscripcionesFragment extends Fragment {
     private ArrayList<SuscripcionesCardItem> listItems;
     private ArrayList<String> listNombres;
     private RecyclerView recyclerView;
+    private String correo;
     private User u;
     private ImageView imgLogo;
     private ViewModelMain vm;
@@ -89,7 +93,11 @@ public class SuscripcionesFragment extends Fragment {
         vm = new ViewModelProvider(getActivity()).get(ViewModelMain.class);
         listItems = new ArrayList<SuscripcionesCardItem>();
 
-        u=vm.getUserById(((MainActivity)getActivity()).getUser().getCorreo());
+        if (((MainActivity)getActivity()).getUserCorreo() != null) {
+            correo = ((MainActivity)getActivity()).getUserCorreo();
+        }
+        u = vm.getUserById(correo);
+
         listNombres = u.getListSuscripciones();
         if(!listNombres.isEmpty()){
             for(int i=0; i<listNombres.size();i++){
@@ -99,9 +107,12 @@ public class SuscripcionesFragment extends Fragment {
             }
 
         }
-        SuscripcionesItemAdapter adapter = new SuscripcionesItemAdapter(listItems,u,vm);
+        SuscripcionesItemAdapter adapter = new SuscripcionesItemAdapter(listItems, u, vm);
+
         recyclerView.setAdapter(adapter);
 
         return view;
     }
+
+
 }
